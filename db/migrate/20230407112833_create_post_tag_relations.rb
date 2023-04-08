293f4266d324e01a2a2f7c1703705702e67aa3b6
EC2 Default User <ec2-user@ip-172-31-21-177.ap-northeast-1.compute.internal>
2023-04-08 10:31:46 +0000

    やり直し

--- db/migrate/20230407112833_create_post_tag_relations.rb
+++ db/migrate/20230407112833_create_post_tag_relations.rb
@@ -1,4 +1,4 @@
-class CreatePostTagRelations < ActiveRecord::Migration[7.0]
+class CreatePostTagRelations < ActiveRecord::Migration[6.1]
   def change
     create_table :post_tag_relations do |t|
       t.references :post, null: false, foreign_key: true
