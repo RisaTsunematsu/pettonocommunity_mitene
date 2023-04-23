6651e43b1f8e2b80df4ec221cefaa9f27c98754b
EC2 Default User <ec2-user@ip-172-31-21-177.ap-northeast-1.compute.internal>
2023-04-23 14:46:04 +0000

    seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,17 @@ Tag.create([
   { name: 'ハムスター' },
   { name: 'その他のペット' }
 ])
+
+users = User.create!(
+  [
+    {first_name: '山田', last_name: '花子', first_name_kana: 'ヤマダ', last_name_kana: 'ハナコ', handlename: 'ハナ', email: 'test@mail.com', password: '000000', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/pf_sample_user_youngwoman.png"), filename:"pf_sample_user_youngwoman.png")}
+    
+  ]
+)
+
+Post.create!(
+  [
+    {post_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/pf_sample_post_situke.jpeg"), filename:"pf_sample_post_situke.jpeg"), title: 'しつけ教室', tag: '#愛犬 #しつけ教室', body: '今日はしつけ教室に行きました。', user_id: users[0].id }
+    
+  ]
+)
