02a14028f8b49a36c971491863fa8f2df1c27883
RisaTsunematsu <purachan1818@gmail.com>
2023-04-29 13:12:09 +0000

    いいね一覧の非同期

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -10,6 +10,11 @@ class Public::LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: @post.id)
     like.destroy if like
+    user = current_user
+    post_ids = user.likes.pluck(:post_id)
+    # post_ids = Like.where(user_id: params[:user_id]).pluck(:post_id) #全部のいいねからuser_idカラムの値がparams[:user_id]のlikeをwhereで絞り込む
+    # いいねを外した時のいいね一覧を定義している
+    @like_posts = Post.where(id: post_ids).page(params[:page]).per(9).order(created_at: :desc)
   end
 end
 
