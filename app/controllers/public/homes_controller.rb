91cf7efb475baac579ea48d105ac469e97e58444
RisaTsunematsu <purachan1818@gmail.com>
2023-04-30 11:34:12 +0000

    headerの仕様の変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,8 @@ class Public::HomesController < ApplicationController
 
 
 def top
- @posts = Post.page(params[:page]).per(30).order(created_at: :desc)
+ @posts = Post.page(params[:page]).per(9).order(created_at: :desc)
+ @user = current_user
 end
 
 
